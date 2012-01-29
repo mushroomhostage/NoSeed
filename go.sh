ea7533fc0b36872eae106696c26d57321344764b
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-29 14:36:50 -0800

    Remove the JavaPlugin until it proves its worth (listen to custom event to change seed?)

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-#CLASSPATH=../craftbukkit-1.1-R2.jar javac *.java -Xlint:deprecation -Xlint:unchecked
+#CLASSPATH=../craftbukkit-1.1-R2.jar javac GetLost.java -Xlint:deprecation -Xlint:unchecked
 javac GetLostLoader.java
 rm -rf me
 mkdir -p me/exphc/GetLost
